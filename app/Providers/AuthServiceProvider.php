07b285b3bce25c485fcdb4fc2b66071dfab8ff8b
oshaman <reg_forall@bigmir.net>
2017-03-28 19:41:02 +0300

    install

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Oshaman\Publication\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'Oshaman\Publication\Model' => 'Oshaman\Publication\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
---
+++ config/settings.php
@@ -0,0 +1,27 @@
+<?php
+	return [
+		'theme' => env('THEME','default'),
+        'maps_api' => '',
+		'captcha_key' => '',
+        
+        
+		'home_port_count' => 5,
+		'home_articles_count' => 3,
+		'paginate' => 2,
+		'recent_comments' => 3,
+		'articles_img' => [
+						'max' => ['width'=>816,'height'=>282],
+						'mini' => ['width'=>55,'height'=>55]
+						
+						],
+		
+		'image' => [
+					'width'=>1024,
+					'height'=>768
+				],				
+		
+	
+	
+	
+	];
+?>
